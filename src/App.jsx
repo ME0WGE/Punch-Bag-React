25e6216d71e270dc34ecd55f3aa6a0bd22a9160b
Kamil Baldyga <baldygakamil@gmail.com>
2025-05-12 11:52:44 +0200

    position bar

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <div className="title">
         <h1 className="h1-title">Punch Bag</h1>
       </div>
+      <ProgressBar />
     </>
   );
 }
--- src/components/progressBar/ProgressBar.jsx
+++ src/components/progressBar/ProgressBar.jsx
@@ -1,5 +1,14 @@
 import "./progressBar.css";
 
 export default function ProgressBar() {
-  return;
+  return (
+    <>
+      <h3 className="h3-progressbar">Progress Bar:</h3>
+      <div className="progressbar-container">
+        <div className="progress-body">
+          <div className="progress-content"></div>
+        </div>
+      </div>
+    </>
+  );
 }
